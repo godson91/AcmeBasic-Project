b6a195056825da26c6cec4a22ead38d08b1a4303
admin <aborjrcl@gmail.com>
2019-11-24 19:45:54 -0600

    basic Single Page App for practice on fundamentals

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient} from '@angular/common/http';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+    
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+
+
+    ]),
+    ProductModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
